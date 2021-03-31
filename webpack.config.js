4fe06ab5fa4f517d060b56f6f7e9b38e98de3829
Sandorik00 <sasha.kobec00@gmail.com>
2021-04-01 02:12:02 +0300

    too lazy for proper webpack setup. So I just turn it to prodaction for now

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
-  mode: 'development',
+  mode: 'production',
   entry: './src/index.tsx',
   plugins: [
     new CleanWebpackPlugin(),
